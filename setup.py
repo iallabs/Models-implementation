981ba941256069cc048c8626520b636df0a8bf2c
Alaa El Bouchti <alaa_23@hotmail.fr>
2018-08-27 18:02:30 +0200

    reorganizing the project // adding mura & chest folders

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(name='Models-implementation',
       version='1.0',
       description='ML Project Medtune',
       author='Amine HILALY & Alaâ EL BOUCHTI',
-      packages=['DenseNet', 'Densenet.preprocessing','Densenet.nets', 'serving', 'utils', 'visualization'],
+      packages=['DenseNet', 'Densenet.preprocessing','Densenet.nets', 'serving', 'utils', 'visualization', 'mura','chest'],
      )
