ea13ad164624046b558fb0856f8d1d1c7447fc98
Alaa El Bouchti <alaa_23@hotmail.fr>
2018-09-03 10:27:42 +0200

    changing config to keep all ckpt

--- estimator.py
+++ estimator.py
@@ -159,7 +159,7 @@ def main():
     #Define max steps:
     max_step = num_epochs*num_batches_per_epoch
     #Define configuration non-distributed work:
-    run_config = tf.estimator.RunConfig(model_dir=train_dir, save_checkpoints_steps=num_batches_per_epoch)
+    run_config = tf.estimator.RunConfig(model_dir=train_dir, save_checkpoints_steps=num_batches_per_epoch, keep_checkpoint_max=None)
     train_spec = tf.estimator.TrainSpec(input_fn=lambda:input_fn(tf.estimator.ModeKeys.TRAIN,
                                                 dataset_dir,file_pattern,
                                                 file_pattern_for_counting, labels_to_name,
