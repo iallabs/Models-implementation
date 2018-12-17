bfe46f19d3d6d4b04121cfae351515d5676f9f69
Alaa El Bouchti <alaa_23@hotmail.fr>
2018-12-17 18:02:49 +0100

    Per_channel mean and standard deviation implematation using tf.data.Dataset

--- utils/images/visu_tensorflow.py
+++ utils/images/visu_tensorflow.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import os
 
-def load_images(filenames_pattern, image_extension='jpg'):
+def load_images(filenames_pattern, num_channels, image_extension='jpg'):
     """
     we want to load data into Datasets in order to perform
     different map-fn fucntions preprocessing
@@ -12,9 +12,10 @@ def load_images(filenames_pattern, image_extension='jpg'):
     Return:
     tf.data.Dataset containing raw image data, decoded and flattened
     """
+    #NOTE: shuffle=false prevent mistakes during labeling images
     dataset = tf.data.Dataset.list_files(filenames_pattern, shuffle=False)
     label_dataset = dataset.map(lambda x: extract_label(x, label_pos=-2), num_parallel_calls=100)
-    image_dataset = dataset.map(lambda x: extract_image(x, image_extension), num_parallel_calls=100)
+    image_dataset = dataset.map(lambda x: extract_image(x, num_channels), num_parallel_calls=100)
     return image_dataset, label_dataset
 
 def extract_label(filename, label_pos=-1):
@@ -27,12 +28,14 @@ def extract_label(filename, label_pos=-1):
     label = tf.string_split([filename_split[label_pos]], delimiter = "_").values[-1]
     return label
 
-def extract_image(filename, image_extension='jpg'):
+def extract_image(filename, num_channels):
+    """
+    Given a dataset of image's filenames and num_chennels per image, we
+    read, decode and convert the image dtype
+    """
     image_raw = tf.read_file(filename)
-    if image_extension == 'jpg':
-        image = tf.image.decode_jpeg(image_raw)
-    else:
-        image = tf.image.decode_png(image_raw)
+    image = tf.image.decode_image(image_raw, num_channels)
+    image = tf.image.convert_image_dtype(image, tf.float32)
     return image
 
 def per_pixel_mean(dataset):
@@ -40,7 +43,10 @@ def per_pixel_mean(dataset):
     Compute the mean of each pixel over the entire dataset.
 
     """
-    pass
+    def collect_pixel():
+        return
+
+    return
 
 def per_pixel_stddev(dataset):
     """
@@ -49,19 +55,25 @@ def per_pixel_stddev(dataset):
     """
     pass
 
-def per_channel_mean(dataset):
+def per_channel_mean_stddev(dataset):
     """
-    Compute the mean of each channelf for every image.
+    Compute the mean & stddev of each channel for every image.
+    """
+    def channel_mean_stddev(decoded_image):
+        means = tf.reduce_mean(decoded_image, axis=[0,1])
+        stddev = tf.sqrt(tf.reduce_mean(tf.square(decoded_image-means), axis=[0,1]))
+        return tf.stack([means, stddev])
+    return dataset.map(lambda x: channel_mean_stddev(x))
 
+def per_mean_stddev(dataset):
     """
-    pass
-
-def per_channel_stddev(dataset):
+    Compute the mean & stddev of every image.
     """
-    Compute the stddev of each channel for every image.
-
-    """
-    pass
+    def mean_stddev(decoded_image):
+        means = tf.reduce_mean(decoded_image)
+        stddev = tf.reduce_mean(tf.sqrt(tf.pow(decoded_image-means,2)))
+        return tf.stack([means, stddev])
+    return dataset.map(lambda x: mean_stddev(x))
 
 def encode_stats(alpha):
     """
@@ -70,10 +82,12 @@ def encode_stats(alpha):
     """
     pass
 
-a, b = load_images("D:/MURA-v1.1/train/*/*/*/*.png", 'png')
+a, b = load_images("D:/MURA-v1.1/train/*/*/*/*.png", 3 ,image_extension='png')
+a= per_channel_mean_stddev(a)
 a_iter = a.make_one_shot_iterator()
 image = a_iter.get_next()
 b_iter = b.make_one_shot_iterator()
 label = b_iter.get_next()
 with tf.Session() as sess:
-    print(sess.run([image, label]))
+    for i in range(10):
+        print(sess.run([image, label]))
--- utils/text/data_utils_text.py
+++ utils/text/data_utils_text.py
@@ -3,6 +3,7 @@ import os
 import sys
 import math
 from utils.data_utils import int64_feature, bytes_feature, float_feature
+#import nltk
 
 
 def read_text_file(filenames, header=False):
